2b94c44b8ec85b0a812e3ffd9b47c8d62d9667bf
valleykid <valleykiddy@gmail.com>
2021-04-14 16:09:09 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,21 @@
+## icatjs-micro-example
+
+A simple example of how to use @icatjs/micro.
+
+This repository hopefully will be helpful for starting your own @icatjs/micro project. It has two react applications(online website) and a vue application(online website) coexisting in a minimalistic way.
+
+## Versions of node/npm
+
+Node v8.10.0 npm 6.1.0
+
+## How to use it
+
+Clone this project
+
+`yarn install` (or `npm install`)
+
+`yarn start` (or `npm start`)
+
+Open up [http://localhost:8000](http://localhost:8000) in a web browser.
+
+![](./demo.gif)
---
+++ src/bundle.ts
@@ -0,0 +1,28 @@
+// @ts-nocheck
+import { registerSubapps, start } from '@icatjs/micro';
+import { markMan } from 'mark-man';
+
+markMan.init({
+  keywords: ['平台', '腾讯'],
+  getDescription: (k) => `自定义描述 ${k}`,
+});
+
+/* 
+// <script src="https://unpkg.com/mark-man@0.0.1/lib/index.js"></script>
+// 直接引用脚本，使用下面的方式调用插入。因为 window.markMan = { markMan, MarkMan }
+
+markMan.markMan.init({
+  keywords: ['平台', '腾讯'],
+  getDescription: (k) => `自定义描述 ${k}`,
+});
+*/
+
+registerSubapps([
+  {
+    name: 'tx',
+    entry: 'https://open.tencent.com/',
+    rules: [{ rule: '/' }],
+  },
+]);
+
+start();
